f17f02d2d482b31664263a23129b2c8f5086d7ea
Vivek <ojhavivek24@gmail.com>
2024-10-25 01:22:20 +0530

    added auth route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import Auth from './routes/auth.js';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.get("/test",(req,res)=>{
     res.status(200).json({"msg":"working"});
     return;
 })
+
+app.use("/auth", Auth);
 
 app.listen(3000,()=>{
     console.log("Server is running");
