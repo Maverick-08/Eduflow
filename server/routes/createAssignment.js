a1383c660b65462007cb8bb9688b4a675ff4e23b
aayu17 <nextcoders.89@gmail.com>
2024-10-29 13:48:16 +0530

    removed empty files

--- server/routes/createAssignment.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createAssignmentHandler } from "../controllers/createAssignmentController.js";
-
-const router = Router();
-
-router.post("/", createAssignmentHandler)
-
-export default router;
