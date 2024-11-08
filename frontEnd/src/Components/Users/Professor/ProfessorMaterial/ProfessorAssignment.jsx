e0be8e13d83ed5399ab99351930e66a60f1b2cb0
aayu17 <nextcoders.89@gmail.com>
2024-11-08 14:14:14 +0530

    disable prev dates

--- frontEnd/src/Components/Users/Professor/ProfessorMaterial/ProfessorAssignment.jsx
+++ frontEnd/src/Components/Users/Professor/ProfessorMaterial/ProfessorAssignment.jsx
@@ -134,6 +134,7 @@ export default function ProfessorAssignment() {
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 value={dueDate}
                 onChange={handleDueDateChange}
+                min={new Date().toISOString().split("T")[0]} // Disable past dates
               />
             )}
             <div>
